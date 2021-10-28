c07ec28fe20604eaa2d5a321745c07267d294666
Fuyu Zhang <iamnickchang@hotmail.com>
2021-10-28 16:03:36 -0500

    Update

--- getFileFromDB.py
+++ getFileFromDB.py
@@ -1,23 +1,35 @@
 import boto3
 import json
+from botocore.exceptions import ClientError
 
 def get_file(event, context):
     client = boto3.client('dynamodb')
-    response = client.get_item(
-        TableName = 'FileServiceDB',
-        Key={
-            "fileId": {'S' : event['fileId']}
+    try:
+        response = client.get_item(
+            TableName = 'FileServiceDB',
+            Key={
+                "fileId": {'S' : event['fileId']}
+            }
+        )
+    except ClientError as e:
+        return{
+            'statusCode': 400,
+            'body': json.dumps(e.response['Error']['Message']),
+            'headers': {
+                "Access-Control-Allow-Origin": "*", 
+                "Content-type": "application/json"
+            }
         }
-    )
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response),
-        'headers': {
-            "Access-Control-Allow-Origin": "*", 
-            "Content-type": "application/json"
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response),
+            'headers': {
+                "Access-Control-Allow-Origin": "*", 
+                "Content-type": "application/json"
+            }
         }
-    }
-    
+
 # front-end sends a request to API GATEWAY
 # get_file is invoked by API GATEWAY, get data from DynamoDB
 # get_file sends a response back to the front-end
--- getUserFromDB.py
+++ getUserFromDB.py
@@ -1,22 +1,34 @@
 import boto3
 import json
+from botocore.exceptions import ClientError
 
 def get_user(event, context):
     client = boto3.client('dynamodb')
-    response = client.get_item(
-        TableName = 'UserServiceDB',
-        Key={
-            "userId": {'S' : event['userId']}
+    try:
+        response = client.get_item(
+            TableName = 'UserServiceDB',
+            Key={
+                "userId": {'S' : event['userId']}
+            }
+        )
+    except ClientError as e:
+        return{
+            'statusCode': 400,
+            'body': json.dumps(e.response['Error']['Message']),
+            'headers': {
+                "Access-Control-Allow-Origin": "*", 
+                "Content-type": "application/json"
+            }
         }
-    )
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response),
-        'headers': {
-            "Access-Control-Allow-Origin": "*", 
-            "Content-type": "application/json"
-        },
-    }
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response),
+            'headers': {
+                "Access-Control-Allow-Origin": "*", 
+                "Content-type": "application/json"
+            }
+        }
 
 # front-end sends a request to API GATEWAY
 # get_user is invoked by API GATEWAY, get data from DynamoDB
